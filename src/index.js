2d64937764955a6ba81381faa36925807971d8fe
Eric w <none>
2023-09-10 14:31:56 -0700

    fixing up initial errors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
-import App from "./App";
+// import App from "./App";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(<h1>This is a test</h1>);
